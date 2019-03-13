3316533193b6ad64947c7e767fa2d25787ab3bea
dicky hardiansyah <dickyhardiansyah12@gmail.com>
2019-03-13 13:39:41 +0700

    add in mainactivity

--- app/src/main/java/me/dicky/selapang/MainActivity.java
+++ app/src/main/java/me/dicky/selapang/MainActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+/**
+ * Create with dickyhh
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
