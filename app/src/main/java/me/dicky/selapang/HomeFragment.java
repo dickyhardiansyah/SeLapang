96f0026054c9337c72133c709c916be93b8d6c71
dicky hardiansyah <dickyhardiansyah12@gmail.com>
2019-03-13 13:22:00 +0700

    Initial commit

---
+++ app/src/main/java/me/dicky/selapang/HomeFragment.java
@@ -0,0 +1,33 @@
+package me.dicky.selapang;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button button = view.findViewById(R.id.btnSearch);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+
+
+
+}
---
+++ app/src/main/java/me/dicky/selapang/TransaksiFragment.java
@@ -0,0 +1,20 @@
+package me.dicky.selapang;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TransaksiFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_transaksi, container, false);
+
+    }
+}
